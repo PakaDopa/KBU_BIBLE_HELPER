8c968297b635ea3ba98af17c31084b370598a789
a-a <detols33@naver.com>
2024-09-10 00:41:26 +0900

    [ADD] TitleScene

--- Assets/Samples/Toolbar Extender/1.4.2/Scene switcher buttons/Editor/SceneSwitcher.cs
+++ Assets/Samples/Toolbar Extender/1.4.2/Scene switcher buttons/Editor/SceneSwitcher.cs
@@ -41,7 +41,7 @@ namespace UnityToolbarExtender.Examples
 		{
             GUILayout.FlexibleSpace();
 
-			string[] bntText = { "AwakeScene", "LobbyScene", "TutorialScene", "MainGameScene" };
+			string[] bntText = { "AwakeScene", "TitleScene", "LobbyScene", "TutorialScene", "MainGameScene" };
 			for (int i = 0; i < bntText.Length; i++)
 				CreateButton(bntText[i], bntText[i]);
 		}

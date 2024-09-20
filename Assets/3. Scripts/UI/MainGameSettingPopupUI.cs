368f7443757fcc48dcf7c921dbcdcf83e835580a
pakadopa <detols33@naver.com>
2024-09-20 11:49:57 +0900

    !

--- Assets/3. Scripts/UI/MainGameSettingPopupUI.cs
+++ Assets/3. Scripts/UI/MainGameSettingPopupUI.cs
@@ -1,6 +1,6 @@
 using DG.Tweening;
-using System;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 using Utils;
 
@@ -9,13 +9,14 @@ public class MainGameSettingPopupUI : MonoBehaviour
     [Header("설정 옵션들")]
     [SerializeField] private RectTransform[] settingRectTransform;
 
+    [Header("버튼 Event관리")]
+    [SerializeField] private Button[] problemCountBnts;
+    [SerializeField] private Button[] typeBnts;
+
     [Header("게임 설정 값")]
     [SerializeField] private int settingCount;          //결정된 문제 수
     [SerializeField] private TestamentType problemType;   //성경 문제 타입 (전체, 구약, 신약)
 
-    private void Awake()
-    {
-    }
     private void Start()
     {
         ShowOption(0);
@@ -23,6 +24,10 @@ public class MainGameSettingPopupUI : MonoBehaviour
     public void OnClickBnt(int ind)
     {
         ShowOption(ind);
+    }
+    public void OnConfirmBnt()
+    {
+
     }
     private void ShowOption(int ind)
     {
@@ -36,6 +41,6 @@ public class MainGameSettingPopupUI : MonoBehaviour
     {
         Sequence seq = DOTween.Sequence();
         seq.SetAutoKill(true);
-        seq.Append(rectTransform.DOAnchorPosX(0, 0.5f));
+        seq.Append(rectTransform.DOAnchorPosX(0, 0.5f).SetEase(Ease.InOutElastic));
     }
 }
